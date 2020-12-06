2c225998bd9bc26e7cc2452714d3ae1484ee618d
Amasterw <agrwash@gmail.com>
2020-12-06 18:28:31 -0500

    Update 6

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -17,10 +17,15 @@ var smokes = [];
 
 
 //create wrapper
-var svg = d3.select("#scatter")
-    .append("svg")
-    .attr("width", svgWidth)
-    .attr("height", svgHeight);
+var chart = d3
+    .select('#scatter')
+    .append('div')
+    .classed('chart', true);
+
+//append an svg element to the chart 
+var svg = chart.append('svg')
+    .attr('width', svgWidth)
+    .attr('height', svgHeight);
 
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
@@ -48,32 +53,37 @@ d3.csv("data.csv").then(function(data, err){
     
     var bottomAxis = d3.axisBottom(Xscale);
     var leftAxis = d3.axisLeft(Yscale);
-
+    //append y axis
     chartGroup.append("g")
         .classed("y-axis", true)
         .call(leftAxis);
-
+    // append x axis
     chartGroup.append("g")
         .classed("x-axis", true)
         .attr("transform", `translate(0, ${height})`)
         .call(bottomAxis);
     
-    circles = svg
-        .selectAll("circle")
-        .data(data, function(d){return d["Smokes"];});
+    //circles = svg
+        //.selectAll("circle")
+        //.data(data, function(d){return d["Smokes"];});
     
     //chartGroup.append("g")
         //.selectAll("dot")
         //.data(data)
-    circles
+    chartGroup.selectAll("dot")
+        .data(data)
         .enter()
         .append("circle")
-        .attr("cx", function (d) {return bottomAxis(d.age);})
-        .attr("cy", function (d) {return leftAxis(d.smokes);} )
+        .attr("cx", d => Xscale(d[age]))
+        //.attr("cx", function (d) {return bottomAxis(d.age);})
+        .attr("cy", d => Yscale(d.smokes))
+        //.attr("cy", function (d) {return leftAxis(d.smokes);} )
         .attr("r", 1.5)
         .style("fill", "#69b3a2")
     //function renderCircles(circlesGroup, Xscale){
     //circlesGroup.transition()
     //.duration(1000)
     //.attr("cx", d => d[chosenXAxis])
+}).catch(function(error) {
+    console.log(error);
 });
